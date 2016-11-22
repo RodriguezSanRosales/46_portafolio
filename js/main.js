272a3d7321e193fe3b772246a7eb06efd5bcf683
RodriguezSanRosales <san.rodzrosales@gmail.com>
2016-11-22 18:16:08 +0000

    actualizacion de fotos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 
 
 $(document).ready(function(){
-    $('.movible').hover(function() {
-        $(this).animate({
-            paddingLeft: "60px"
-        },{
-            duration: 1000,
-        });
-    };
-    function() {
-        $(this).animate({
-            paddingLeft: "5px"
-        },{
-            duration: 1000,
-        });
+    // $('.movible').hover(function() {
+    //     $(this).animate({
+    //         paddingLeft: "60px"
+    //     },{
+    //         duration: 1000,
+    //     });
+    // };
+    // function() {
+    //     $(this).animate({
+    //         paddingLeft: "5px"
+    //     },{
+    //         duration: 1000,
+    //     });
 
     $('[data-toggle="tooltip"]').tooltip();
 });
